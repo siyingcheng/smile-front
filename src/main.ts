6ffbfd890479eb44323c5aea0b3a56c27d6ebd13
siyingcheng <283736566@qq.com>
2023-10-31 19:34:45 +0800

    init project with vue@cli; add element-plus by on-demand; configurate auto import

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routers'
-// import './style.css'
 import { createPinia } from 'pinia'
 
-createApp(App).use(router).use(ElementPlus).use(createPinia()).mount('#app')
+import App from './App.vue'
+import router from './router'
 
-import { useUserStore } from '@/stores/UserStore'
-const userStore = useUserStore()
-await userStore.loadLocalUser()
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue', // import {ref, reactive, toRef, ...} from 'vue'
+        'vue-router',
+        // custom
+        {
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'], // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          axios: [
+            // default imports
+            ['default', 'axios'], // import { default as axios } from 'axios',
+          ],
+        },
+      ],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
